fc62fe7752a6b1aab7a23cacf78ec6774c2af623
Gonzalo Rodriguez <g@stm.pm>
2017-09-05 18:30:43 -0500

    changed to hero header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,12 +4,14 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	@yield('title')
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/css/ionicons.min.css">
     <link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
+		<link rel="stylesheet" href="/assets/css/herohead.css">
     <link rel="stylesheet" href="/assets/css/main.css">
+
 </head>
 <body>
 	@yield('content')
@@ -18,13 +20,13 @@
 		integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 		crossorigin="anonymous">
 	</script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="/assets/js/bootstrap.min.js"></script>
 	<script src="/assets/js/jquery.appear.min.js"></script>
     <script src="/assets/js/jquery.incremental-counter.js"></script>
     <script src="/assets/js/script.js"></script>
+		<script src="/assets/js/herohead.js"></script>
     <script src="/assets/js/main.js"></script>
-    
-        
+
+
 </body>
 </html>
- 
