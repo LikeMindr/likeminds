77cb6a3633af6f6964a192e67de45c0347667de3
Alex Endacott <endacott.alex@gmail.com>
2017-09-08 14:08:26 -0500

    add collumn to seeder

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -15,11 +15,13 @@ class EventTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker\Factory::create();
+		$categories = ["music", "happy hours", "art", "film theater", "dance"];
 
         for($i = 0; $i <= 500; $i++) {
         	$event = new Event();
         	$event->description = $faker->bs;
         	$event->title = $faker->catchPhrase;
+			$event->category = $categories[mt_rand(0, 4)];
 			$event->date = $faker->bs;
         	$event->location = $faker->bs;
         	$event->created_by = User::all()->random()->id;
