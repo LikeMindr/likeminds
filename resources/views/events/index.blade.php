b11810bf32bb8759c28badef7f5c837afbb4b684
Alex Endacott <endacott.alex@gmail.com>
2017-09-08 14:01:50 -0500

    build out events index

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -87,11 +87,14 @@
 						</h3>
 						<div class="creative_filds_block">
 							<div class="ul">
-								<a  data-filter=".category-1" class="filter">Graphic Design		</a>
-								<a data-filter=".category-2" class="filter">Photography			</a>
+								@foreach($trends as $trend)
+								<a  data-filter=".category-1" class="filter">{{ $trend }}</a>
+								@endforeach
+						<!--		<a data-filter=".category-2" class="filter">Photography			</a>
 								<a data-filter=".category-3" class="filter">Interaction Design	</a>
 								<a data-filter=".category-4" class="filter">Art Direction		</a>
 								<a data-filter=".category-5" class="filter">Illustration		</a>
+						-->
 							</div>
 						</div>
 					</div>
