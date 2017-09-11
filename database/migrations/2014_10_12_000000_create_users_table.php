3fdbdda807f580677ed463cbcdac6ba8e9d31b2f
Alex Endacott <endacott.alex@gmail.com>
2017-09-11 11:51:03 -0500

    update migrations and seeders

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,18 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
+			$table->string('location');
+			$table->string('avatar');
+			$table->text('bio');
+			$table->string('question');
+			$table->string('answer');
+			$table->string('preference1');
+			$table->string('preference2');
+			$table->string('preference3');
+			$table->string('facebook');
+			$table->string('twitter');
+			$table->string('pinterest');
+			$table->string('instagram');
             $table->rememberToken();
             $table->timestamps();
         });
