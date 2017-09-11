cb05bf59a36c28c26de43606d9ec0785dc3bfa00
Alex Endacott <endacott.alex@gmail.com>
2017-09-11 14:02:35 -0500

    users can now sign up for events

--- database/migrations/2017_09_06_145343_create_attends_table.php
+++ database/migrations/2017_09_06_145343_create_attends_table.php
@@ -17,12 +17,6 @@ class CreateAttendsTable extends Migration
 			$table->increments('id');
 			$table->integer('user_id')->unsigned();
 			$table->integer('event_id')->unsigned();
-			$table->foreign('user_id')
-				->references('id')
-				->on('users');
-			$table->foreign('event_id')
-				->references('id')
-				->on('events');
 		});
 
     }
@@ -34,20 +28,6 @@ class CreateAttendsTable extends Migration
      */
     public function down()
     {
-		
-		Schema::table('attends', function($table)
-		{
-			$table->dropForeign(['user_id']);
-
-		});
-
-		
-		Schema::table('attends', function($table)
-		{
-			$table->dropForeign(['event_id']);
-
-		});
-
         Schema::drop('attends');
     }
 }
