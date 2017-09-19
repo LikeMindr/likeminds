37939e1f1bcda9d0c46dabeec0320e8b9c2b29fa
Alex Endacott <endacott.alex@gmail.com>
2017-09-19 12:30:54 -0500

    build out event page

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -26,46 +26,92 @@
                     <div class="col-xs-12 col-md-4 left-feild"> 
                         <div class="be-user-block style-3" style="background: #EAEEF6;"> 
                             <div class="be-user-detail"> 
-                                <a class="be-ava-user style-2" href="page1.html"> 
-                                    <img src="img/ava_10.jpg" alt=""> 
-                                </a>                                                                                                   
+						<a class="be-img-block" href="/accounts/{{$event['user']['id']}}">
+							<img id="host-img" src=
+								<?php clearstatcache();
+									if($event['user']['image'] != NULL): ?>
+										"/assets{{$event['user']['image']}}"
+								<?php else: ?>
+										"/assets/img/usericon.png"
+								<?php endif; ?>
+								alt="{{ $event['user']['name'] }}" class="img-responsive">
+						</a>
                                 <p class="be-use-name" style="color: #2C4762;">Leigh Taylor</p> 
                                 <div class="be-user-info" style="font-family: 'Montserrat', sans-serif;;;"> 
-                                    Barnsley, United Kingdom
+								{{ $event['user']['location'] }}
 </div>                                                                  
                                 <div class="be-user-social"> 
-                                    <a class="social-btn color-1" href="page1.html"><i class="fa fa-facebook"></i></a> 
-                                    <a class="social-btn color-2" href="page1.html"><i class="fa fa-twitter"></i></a> 
-                                    <a class="social-btn color-3" href="page1.html"><i class="fa fa-google-plus"></i></a> 
-                                    <a class="social-btn color-4" href="page1.html"><i class="fa fa-pinterest-p"></i></a> 
-                                    <a class="social-btn color-5" href="page1.html"><i class="fa fa-instagram"></i></a> 
-                                    <a class="social-btn color-6" href="page1.html"><i class="fa fa-linkedin"></i></a> 
+								<?php if(strlen($event['user']['facebook']) > 21): ?>					
+								<a class="social-btn color-1" 
+									href="{{$event['user']['facebook']}}" target="blank">
+									<i class="fa fa-facebook"></i></a>
+								<?php endif; ?>
+								<?php if(strlen($event['user']['twitter']) > 20): ?>
+								<a class="social-btn color-2" 
+									href="{{$event['user']['twitter']}}" target="blank">
+									<i class="fa fa-twitter"></i></a>
+								<?php endif;  ?>
+								<?php if(strlen($event['user']['pinterest']) > 22): ?>
+								<a class="social-btn color-4" 
+									href="{{$event['user']['pinterest']}}" target="blank">
+									<i class="fa fa-pinterest-p"></i></a>
+								<?php endif;  ?>
+								<?php if(strlen($event['user']['instagram']) > 22): ?>
+								<a class="social-btn color-5" 
+									href="{{$event['user']['instagram']}}" target="blank">
+									<i class="fa fa-instagram"></i></a>
+								<?php endif;  ?>
                                 </div>                                                                  
                             </div>                             
                             <div class="be-user-statistic"> 
                                 <div class="stat-row clearfix" style="font-family: 'Montserrat', sans-serif;background: #89C12A;;;">
-                                    <i class="stat-icon icon-views-b"></i> Projects views
-                                    <span class="stat-counter">218098</span>
+                                     Events Created
+                                    <span class="stat-counter">{{count($event['user']->events)}}</span>
                                 </div>                                 
                                 <div class="stat-row clearfix" style="font-family: 'Montserrat', sans-serif;background: #89C12A;;;">
-                                    <i class="stat-icon icon-like-b"></i>Appreciations
-                                    <span class="stat-counter">14335</span>
+                                    Events Attended
+                                    <span class="stat-counter">{{count($event['user']->attends)}}</span>
                                 </div>                                                                                                   
                             </div>                             
-                        </div>                         
+                        </div>
                         <div class="be-desc-block">
                             <div class="be-desc-author" style="background: #EAEEF6;"> 
                                 <div class="be-desc-label" style="font-family: 'Grand Hotel', cursive;color: #2C4762;;;">About the Creator</div>                                 
                                 <div class="clearfix"></div>                                 
-                                <div class="be-desc-text" style="font-family: 'Montserrat', sans-serif;color: #2C4762;;;"> 
-                                    Nam sit amet massa commodo, tristique metus at, consequat turpis. In vulputate justo at auctor mollis. Aliquam non sagittis tortor. Duis tristique suscipit risus, quis facilisis nisl congue vitae. Nunc varius pellentesque scelerisque. Etiam quis massa vitae lectus placerat ullamcorper pellentesque vel nisl.
-</div>                                 
-                            </div>                             
+								<div class="be-desc-text" style="font-family:
+								'Montserrat', sans-serif;color: #2C4762;;;">
+								@if($event['user']['bio'] != NULL)	
+								{{$event['user']['bio']}}
+								@else
+								This likemindr has not yet added a bio.
+								@endif
+								</div>                                 
+                            </div>
                             <div class="be-desc-author" style="background: #EAEEF6;margin-top: 50px;"> 
                                 <div class="be-desc-label" style="font-family: 'Grand Hotel', cursive;color: #2C4762;;;">Like-Minds Attending</div>                                 
                                 <div class="clearfix"></div>                                 
+		<div class="row">
+			<div class="col-md-12">
                                 <div class="be-desc-text" style="color: #2C4762;"> 
-                                    Sed dignissim scelerisque pretium. Vestibulum vel lacus laoreet nunc fermentum maximus. Proin id sodales sem, at consectetur urna. Proin vestibulum, erat a hendrerit sodales, nulla libero ornare dolor.
+							@foreach($event->attends as $user)	
+								<div class="category-1 mix custom-column-5">
+										<a href="/accounts/{{$user['user_id']}}" class="be-img-block">
+											<img src=
+											<?php clearstatcache();
+												if(App\User::find($user['user_id'])->image != NULL): ?>
+												"../assets{{App\User::find($user['user_id'])->image}}"
+											<?php else: ?>
+												"/assets/img/usericon.png"
+											<?php endif; ?>
+							class="img-responsive" alt="{{App\User::find($user['$user_id'])['name']}}">
+										</a>
+										<a href="/accounts/{{$user['user_id']}}">
+										{{App\User::find($user['user_id'])['name']}}
+										</a>
+								</div>
+							@endforeach
+					</div>
+				</div>
 </div>                                 
                             </div>
                         </div>                         
@@ -84,13 +130,18 @@
                                     <div class="row"> 
                                         <div class="col-ml-12 col-xs-6 col-sm-4" style="width: 100%;background: #EAEEF6;padding-top: 20px;margin-left: 10px;"> 
                                             <div class="be-post" style="color: #2C4762;"> 
-                                                <a href="page1.html" class="be-img-block"> 
-                                                    <img src="img/p1.jpg" alt="omg"> 
-                                                </a>                                                 
-
-
+											<img src=
+											<?php clearstatcache();
+												if($event->image != NULL): ?>
+													"/assets{{$event->image}}"
+												<?php else: ?>
+												"{{App\Event::defaultImage($event->category)}}"
+											<?php endif; ?>
+											/>
                                                 <div class="row"> 
-                                                    <h1 style="margin-left: 15px;font-family: 'Montserrat', sans-serif;;;">Event Name </h1>
+													<h1 style="margin-left:
+													15px;font-family: 'Montserrat', sans-serif;;;">
+													{{$event->title}} </h1>
                                                 </div>                                                 
                                                 <div class="row"> 
                                                     <div class="col-md-4"> 
@@ -98,7 +149,7 @@
                                                     </div>                                                     
 
                                                     <div class="col-md-4"> 
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Column title</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">On {{$event->date}} at {{$event->time}}</h3> 
                                                     </div>
                                                 </div>                                                 
 
@@ -110,7 +161,7 @@
 
 		
                                                     <div class="col-md-4"> 
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Column title</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">{{$event->location}}</h3> 
                                                     </div>
                                                 </div>                                                 
 
@@ -120,7 +171,7 @@
                                                     </div>                                                     
 
                                                     <div class="col-md-4"> 
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Column title</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">{{$event->category}}</h3> 
                                                     </div>
                                                 </div>                                                 
                                                 <div class="row"> 
@@ -128,37 +179,96 @@
                                                         <h3 style="font-family: 'Montserrat', sans-serif;;;">Description:</h3> 
                                                     </div>                                                     
                                                     <div class="col-md-4">
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Column title</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">{{$event->description}}</h3> 
                                                         <p> <br></p> 
                                                     </div>
                                                 </div>                                                 
-
                                                 <div class="row"> 
                                                     <div class="col-md-4">
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Description:</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Spots left:</h3> 
                                                     </div>                                                     
                                                     <div class="col-md-4">
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Column title</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">{{ $event->num_people - count($event->attends)}}</h3> 
                                                         <p> <br></p> 
                                                     </div>
                                                 </div>                                                 
-
                                                 <div class="row"> 
                                                     <div class="col-md-4">
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Description:</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Currently signed up:</h3> 
                                                     </div>                                                     
                                                     <div class="col-md-4">
-                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">Column title</h3> 
+                                                        <h3 style="font-family: 'Montserrat', sans-serif;;;">{{ count($event->attends) }}</h3> 
                                                         <p> <br></p> 
                                                     </div>
                                                 </div>                                                 
+												
+						<?php if (Auth::check()) {
+							$user = App\User::find(Auth::id());
+							$attending = false;
+							foreach($user->attends as $element) {
+								if($element['event_id'] == $event->id) {
+									$attending = true;
+								}
+							}
+							}
+							if(Auth::check() && $attending && Auth::id() != $event['user']['id']): ?>
+							<a href="/attends/cancel/{{$event->id}}/{{Auth::id()}}">
+								
+								<button >CANCEL</button>
+								
+							</a>
+					
+								<div title="Add to Calendar" class="addeventatc">
+								    ADD TO CALENDAR
+								    <span class="start">
+								<?php 
+								echo date('m/d/Y', strtotime($event->date))
+									. ' ' .
+									date('g:i A', strtotime($event->time));
+										?>
+									</span>
+								    <span class="end">
+								<?php 
+								echo date('m/d/Y', strtotime($event->date))
+									. ' ' .
+									date('g:i A', strtotime($event->time)+3600);
+										?>
+									</span>
+								    <span class="timezone">America/Chicago</span>
+								    <span class="title">{{$event->title}}</span>
+								    <span class="description">{{$event->description}}</span>
+								    <span class="location">{{ $event->location }}</span>
+								    <span class="organizer">
+									{{App\User::find($event->created_by)->name}}
+									</span>
+								    <span class="organizer_email">
+									{{App\User::find($event->created_by)->email}}
+									</span>
+								    <span class="facebook_event">https://www.facebook.com/events/703782616363133</span>
+								    <span class="all_day_event">false</span>
+								    <span class="date_format">MM/DD/YYY</span>
+								    <span class="client">arhvfcwBtzTYjbzlSmIF31075</span>
+								</div>
+							<?php elseif(Auth::check() && Auth::id() != $event['user']['id']
+										&& $event->num_people - count($event->attends) > 0): ?>
+						<form class="sign-up-event-cal" method="POST" action="{{ action('AttendsController@store') }}">
+							{!! csrf_field() !!}
+							<input type="hidden" value="{{Auth::id()}}" name="user_id">
+							<input type="hidden" value="{{$event->id}}" name="event_id">
+							
+								<button class="form-button" id="submit">REGISTER</button> 
+							
+						</form>
+						<?php endif;  ?>
 
-                                                <div class="row"></div>                                                                                                  
-                                                <div class="author-post"> 
-                                                    <img src="img/a1.png" alt="" class="ava-author"> 
-                                                    <span>by                         <a href="page1.html">Hoang Nguyen</a></span> 
-                                                </div>                                                 
-                                            </div>                                             
+						<?php if(Auth::check() && Auth::id() == $event['user']['id']): ?>
+						<form method="GET"
+							action="{{ action('EventsController@edit', array($event->id)) }}">
+							{!! csrf_field() !!}
+							<button class="form-button" id="submit">EDIT</button>
+						</form>
+						<?php endif; ?>
+											
                                         </div>                                         
                                     </div>                                     
                                 </div>                                 
